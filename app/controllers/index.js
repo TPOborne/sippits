a9dd732d233cf31bbf72ddb41fe34a54ecf4bdcf
TPOborne <tpoborne@gmail.com>
2017-11-09 11:58:03 +0000

    Create Game Node
    Create game now checks if game code exists. If it does, it will not execute. Else, it creates the game and adds the player.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -47,11 +47,24 @@ module.exports = app => {
       return false;
     }
 
-    return db.query('INSERT INTO games (game_code) VALUES (?)', [gameCode])
-      .then(() => {
-        return res.send('success');
+    db.query('SELECT game_id from games WHERE game_code = ? AND active = 1' , [gameCode])
+      .then(result => {
+        if (result.length > 0) {
+          return res.send('Game with that code currently in use');
+        } else {
+          return db.query('INSERT INTO games (game_code) VALUES (?)', [gameCode])
+          .then(() => {
+            return db.query('SELECT game_id FROM games WHERE game_code = ?', [gameCode])
+            .then(result => {
+              let gameId = result[0].game_id;
+              return db.query('INSERT INTO players (player_name, game_id) VALUES (?, ?)', [name, gameId])
+              .then(() => {
+                return res.send('success');
+              })
+            }) 
+          })
+        }
       })
-  })
 
-  
+  })
 }
--- app/models/home.js
+++ app/models/home.js
@@ -15,23 +15,13 @@ exports.getGameIdByCode = (gameCode) => {
         });
 }
 
+exports.getGameCodeById = (gameId) => {
+    return db.query('SELECT game_code FROM games WHERE game_id = ?', [gameId])
+        .then( result => {
+            return result[0];
+        });
+}
+
 exports.insertNewPlayer = (gameId, playerName) => {
     return db.query('INSERT INTO players (game_id, player_name) VALUES (?, ?)', [gameId, playerName]);
 }
-
-
-// return db.query('SELECT game_id FROM games WHERE game_code = ?', [gameCode])
-// .then( result => {
-//   let gameData = result[0];
-
-//   if ( !gameData || !gameData.game_id ) {
-//     return res.send('Invalid Game Code');
-//   }
-
-//   req.session.gameId = gameData.game_id;
-
-//   return db.query('INSERT INTO players (game_id, player_name) VALUES (?, ?)', [gameData.game_id, name])
-//     .then( () => {
-//       return res.send('success');
-//     })
-// })
